4cc6bddb70875b744f18d312748ac5b08bd7da2b
Deep Datta Roy <deepdattaroy8888@gmail.com>
2015-03-02 21:14:41 -0500

    Fixed up fonts

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@ static TextLayer *timeLayer;
 static TextLayer *dateLayer;
 static TextLayer *dayLayer;
 
+
 static void mainWindowInit(Window *window){
   ////////////////Set up window///////////////////////
   //window color
@@ -13,12 +14,12 @@ static void mainWindowInit(Window *window){
   
   ///////////////Set up Time//////////////////////////
   //set text layer
-  timeLayer = text_layer_create(GRect(0,47, 144, 50));
+  timeLayer = text_layer_create(GRect(2,34, 130, 44));
   //set colors
   text_layer_set_background_color(timeLayer, GColorClear);
   text_layer_set_text_color(timeLayer, GColorWhite);
   //set fonts
-  text_layer_set_font(timeLayer, fonts_get_system_font(FONT_KEY_BITHAM_42_MEDIUM_NUMBERS));
+  text_layer_set_font(timeLayer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   text_layer_set_text_alignment(timeLayer, GTextAlignmentLeft);
   
   //add to screen
@@ -26,12 +27,12 @@ static void mainWindowInit(Window *window){
 
   /////////////Set up Date/////////////////////////////////
   //create the text layer
-  dateLayer = text_layer_create(GRect(0, 97, 144, 50));
+  dateLayer = text_layer_create(GRect(2, 78, 130, 32));
   //set colors
   text_layer_set_background_color(dateLayer, GColorClear);
   text_layer_set_text_color(dateLayer, GColorWhite);
   //set fonts
-  text_layer_set_font(dateLayer, fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK));
+  text_layer_set_font(dateLayer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
   text_layer_set_text_alignment(dateLayer, GTextAlignmentLeft);
   
   //add to screen
@@ -39,12 +40,12 @@ static void mainWindowInit(Window *window){
   
   /////////////Set up Day/////////////////////////////////////
   //create text layer
-  dayLayer = text_layer_create(GRect(0,15, 144,50));
+  dayLayer = text_layer_create(GRect(2,2, 130,32));
   //set colors
   text_layer_set_background_color(dayLayer, GColorClear);
   text_layer_set_text_color(dayLayer, GColorWhite);
   //set fonts
-  text_layer_set_font(dayLayer, fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK));
+  text_layer_set_font(dayLayer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
   text_layer_set_text_alignment(dayLayer, GTextAlignmentLeft);
   
   //add to screen
@@ -65,7 +66,7 @@ static void updateTime(){
 
   ///////////////Set up time////////////////////////
   //buffer to hold time
-  static char timeBuffer[] = "00:12A";  
+  static char timeBuffer[] = "00:00A";  
   //format the time if 24 hours style
   if (clock_is_24h_style()){
     strftime(timeBuffer, sizeof(timeBuffer), "%H:%M", curTimeLocal);
@@ -82,9 +83,9 @@ static void updateTime(){
   text_layer_set_text(timeLayer, timeBuffer);
   ////////////Set up Date//////////////////////////////
   //buffer to hold date
-  static char dateBuffer[] = "June 25";
+  static char dateBuffer[] = "December 25";
   //format date
-  strftime(dateBuffer, sizeof(dateBuffer), "%b %e", curTimeLocal);  
+  //strftime(dateBuffer, sizeof(dateBuffer), "%b %e", curTimeLocal);  
     
   //print to screen
   text_layer_set_text(dateLayer, dateBuffer);
@@ -92,7 +93,7 @@ static void updateTime(){
   //buffer
   static char dayBuffer[] = "Wednesday";
   //format day
-  strftime(dayBuffer, sizeof(dayBuffer), "%A", curTimeLocal);
+  //strftime(dayBuffer, sizeof(dayBuffer), "%A", curTimeLocal);
   
   //print to screen
   text_layer_set_text(dayLayer, dayBuffer);    
